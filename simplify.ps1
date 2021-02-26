fb8703dc06e603e72a16cb15a2102d81b96d0e8b
CodeClover <codeclover@yeah.net>
2021-02-26 17:23:16 +0800

    添加windows10简化脚本

---
+++ simplify.ps1
@@ -0,0 +1,23 @@
+# name: simplify.ps1
+# author: codeclover
+# encoding: GB2312
+# desc: 用于删除windows10操作系统自带的软件，最后保留商店、照片和计算器。
+# version: 1.0
+$packageNames = "WindowsStore*", "Windows.Photos_*", "WindowsCalculator_*"
+Clear-Host | Out-Null
+"=" * 64 | Write-Output
+"_" * 64 | Write-Output
+Write-Output "名称：windows 10 简化脚本"
+Write-Output "功能：删除windows10自带软件"
+Write-Output "作者：CodeClover"
+Write-Host "版本：1.0"
+"-" * 64 | Write-Host
+Write-Output "步骤一、卸载所有自带软件："
+Get-AppxPackage -alluser | Remove-AppxPackage | Out-Null
+Write-Output "步骤二、安装软件商店、照片和计算器："
+foreach($packageName in $packageNames){
+    $package = "C:\Program Files\WindowsApps\Microsoft.{0}_x64__8wekyb3d8bbwe\appxmanifest.xml" -f $packageName
+    Add-AppxPackage -register $package  -disabledevelopmentmode | Out-Null
+}
+Write-Host "步骤三、卸载完成，建议重启："
+"=" * 64 | Write-Host
